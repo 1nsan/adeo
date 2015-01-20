d082ea88b20b347fb8ed317e097d03e06a480630
1nsan <flavien.guillocheau@gmail.com>
2015-01-20 15:20:56 +0100

    ok

--- delamaison/db/migrate/20150119153014_create_topics.rb
+++ delamaison/db/migrate/20150119153014_create_topics.rb
@@ -3,7 +3,6 @@ class CreateTopics < ActiveRecord::Migration
     create_table :topics do |t|
       t.string :title
       t.text :content
-      t.integer :user_id
       t.belongs_to :user, index: true
 
       t.timestamps
