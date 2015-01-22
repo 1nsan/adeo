fcae46a1661a3c72c985358443c30b2581b72a1e
1nsan <flavien.guillocheau@gmail.com>
2015-01-22 16:26:40 +0100

    ok

--- delamaison/config/routes.rb
+++ delamaison/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   resources :topics
 
+  root 'items#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
